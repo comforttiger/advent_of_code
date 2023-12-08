28a8967b5ccf3e23d65d2c30bd1dabd43781a27e
lotte <comfortlotte@hotmail.com>
2023-12-08 10:34:03 +0100

    day 8

---
+++ 2023/ruby/day8.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# this makes it easy for me to choose between example or main input w/o editing any code
+print 'enter for input file, ex for example '
+INPUT = "inputs/8#{gets.chomp}".freeze
+print 'part 1 or 2? '
+PART = gets.chomp
+
+# Node class, has a node to its left and right.
+class Node
+  include Comparable
+  attr_reader :value
+  attr_accessor :left, :right
+
+  def initialize(value, left, right)
+    @value = value
+    @left = left
+    @right = right
+  end
+
+  def <=>(other)
+    return @value <=> other.value if other.instance_variable_defined?(:@value)
+
+    @value <=> other
+  end
+end
+
+# get first line of input and split it into separate instructions
+def instructions = File.open(INPUT).first.chomp.split('')
+
+# creates a hash of nodes
+def nodes
+  nodes = {}
+  File.readlines(INPUT).drop(2).each do |line|
+    node, left, right = line.gsub(/[(]|[)]|,|=/, '').split
+
+    nodes.store(node, Node.new(node, left, right))
+  end
+  nodes
+end
+
+# counts steps until node satisfies a condition.
+# &condition lets us pass a block to this method, like {_1 == 'ZZZ' } for part 1
+def count_steps(nodes, instructions, start, &condition)
+  current_node = nodes[start]
+  steps = 0
+  loop do
+    instructions.each do |instruction|
+      return steps if condition.call(current_node)
+
+      current_node = nodes[current_node.left] if instruction == 'L'
+      current_node = nodes[current_node.right] if instruction == 'R'
+      steps += 1
+    end
+  end
+end
+
+# counts steps once from AAA to ZZZ
+def traverse(nodes, instructions)
+  count_steps(nodes, instructions, 'AAA') { _1 == 'ZZZ' }
+end
+
+# counts how many steps it takes for each node ending in A to get to a node ending in Z.
+# they will always get to a node ending in Z at a fixed interval (e.g. every 105 steps), and if we then get
+# the least common multiple for all of these, thats when they all overlap. it kinda makes sense i guess,
+# but i wouldnt have figured it out without reddit.
+def traverse_multiple(nodes, instructions)
+  nodes.keys.filter { _1.end_with?('A') }.map do |node|
+    count_steps(nodes, instructions, node) { _1.value.end_with?('Z') }
+  end.reduce(&:lcm)
+end
+
+puts traverse(nodes, instructions) if PART == '1'
+puts traverse_multiple(nodes, instructions) if PART == '2'
